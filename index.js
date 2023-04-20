19cfc971a306d05bfb1b9616261a6b088d364360
Oninety <131241736+Oninety@users.noreply.github.com>
2023-04-19 17:14:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,95 @@
-const express = require('express')
-const request = require('request')
-const app = express()
+const express = require('express');
+const request = require('request');
+const app = express();
 const fs = require('fs');
-const { promisify } = require('util')
-const readFile = promisify(fs.readFile)
-const GPT_MODE = process.env.GPT_MODE
+const { promisify } = require('util');
+const readFile = promisify(fs.readFile);
+const GPT_MODE = process.env.GPT_MODE;
 
-let file_context = "You are a helpful Twitch Chatbot."
+let file_context = "You are a helpful Twitch Chatbot.";
 
 const messages = [
-  {role: "system", content: "You are a helpful Twitch Chatbot."}
+  { role: "system", content: "You are a helpful Twitch Chatbot." }
 ];
 
-console.log("GPT_MODE is " + GPT_MODE)
-console.log("History length is " + process.env.HISTORY_LENGTH)
-console.log("OpenAI API Key:" + process.env.OPENAI_API_KEY)
+console.log("GPT_MODE is " + GPT_MODE);
+console.log("History length is " + process.env.HISTORY_LENGTH);
+console.log("OpenAI API Key:" + process.env.OPENAI_API_KEY);
 
-app.use(express.json({extended: true, limit: '1mb'}))
+app.use(express.json({ extended: true, limit: '1mb' }));
 
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+  console.log("Just got a request!");
+  res.send('Yo!');
+});
 
-if (process.env.GPT_MODE === "CHAT"){
-
-  fs.readFile("./file_context.txt", 'utf8', function(err, data) {
+if (process.env.GPT_MODE === "CHAT") {
+  fs.readFile("./file_context.txt", 'utf8', function (err, data) {
     if (err) throw err;
-    console.log("Reading context file and adding it as system level message for the agent.")
+    console.log("Reading context file and adding it as system level message for the agent.");
     messages[0].content = data;
   });
-
 } else {
-
-  fs.readFile("./file_context.txt", 'utf8', function(err, data) {
+  fs.readFile("./file_context.txt", 'utf8', function (err, data) {
     if (err) throw err;
     console.log("Reading context file and adding it in front of user prompts:")
     file_context = data;
     console.log(file_context);
   });
-
 }
 
 app.get('/gpt/:text', async (req, res) => {
-    
-    //The agent should recieve Username:Message in the text to identify conversations with different users in his history. 
-    
-    const text = req.params.text
-    const { Configuration, OpenAIApi } = require("openai");
 
-    const configuration = new Configuration({
-      apiKey: process.env.OPENAI_API_KEY,
-    });
+  //The agent should recieve Username:Message in the text to identify conversations with different users in his history.
 
-    const openai = new OpenAIApi(configuration);      
-    
-    if (GPT_MODE === "CHAT"){
-      //CHAT MODE EXECUTION
+  const text = req.params.text;
+  const { Configuration, OpenAIApi } = require("openai");
 
-      //Add user message to  messages
-      messages.push({role: "user", content: text})
-      //Check if message history is exceeded
-      console.log("Conversations in History: " + ((messages.length / 2) -1) + "/" + process.env.HISTORY_LENGTH)
-      if(messages.length > ((process.env.HISTORY_LENGTH * 2) + 1)) {
-          console.log('Message amount in history exceeded. Removing oldest user and agent messages.')
-          messages.splice(1,2)
-     }
-    
-      console.log("Messages: ")
-      console.dir(messages)
-      console.log("User Input: " + text)
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
 
-      const options = {
-        method: 'POST',
-        url: 'https://api.pawan.krd/v1/chat/completions',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
-        },
-        body: {
-          model: 'text-davinci-003',
-          prompt: messages,
-          temperature: 0.5,
-          max_tokens: 128,
-          top_p: 1,
-          frequency: 0
-        },
-        json: true
-      }
+  const openai = new OpenAIApi(configuration);
 
-      console.log("Sending Prompt to OpenAI API: ")
-      console.dir(options.body)
+  if (GPT_MODE === "CHAT") {
+    //CHAT MODE EXECUTION
 
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        console.log("Received response from OpenAI API. Output: ")
-        console.dir(body.choices[0].text)
-        //Add agent response to messages
-        messages.push({role: "system", content: body.choices[
+    //Add user message to messages
+    messages.push({ role: "user", content: text });
+    //Check if message history is exceeded
+    console.log("Conversations in History: " + ((messages.length / 2) - 1) + "/" + process.env.HISTORY_LENGTH);
+    if (messages.length > ((process.env.HISTORY_LENGTH * 2) + 1)) {
+      console.log('Message amount in history exceeded. Removing oldest user and agent messages.');
+      messages.splice(1, 2);
+    }
+
+    console.log("Messages: ");
+    console.dir(messages);
+    console.log("User Input: " + text);
+
+    const options = {
+      method: 'POST',
+      url: 'https://api.pawan.krd/v1/chat/completions',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`
+      },
+      body: {
+        model: 'text-davinci-003',
+        prompt: messages,
+        temperature: 0.5,
+        max_tokens: 128,
+        top_p: 1,
+        frequency: 0
+      },
+      json: true
+    };
+
+    console.log("Sending Prompt to OpenAI API: ");
+    console.dir(options.body);
+
+    request(options, function (error, response, body) {
+      if (error) throw new Error(error);
+      console.log("Received response from OpenAI API. Output: ");
+      console.dir(body.choices[0].text);
+      //Add agent response to
